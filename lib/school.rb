30636e4e4d0f45b66526c1f1d1714df5f406e4a9
Coby <cobyrpilpel2@gmail.com>
2020-12-17 06:10:46 -0500

    commit tree ab8a6d2163f69a55dfa5ceb9c763cd49c12242af

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,39 @@
 # code here!
+class School
+    attr_reader :name, :roster
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+    def roster
+        @roster
+    end
+    
+    def add_student(name,grade)
+    if (roster[grade])
+        roster[grade] << name
+    else
+        roster[grade] = []
+        roster[grade] << name
+    end 
+    
+    def grade(grade)
+        @roster.map do |key, value|
+            if grade == key
+                return value
+            end
+        end
+    end
+
+
+    end
+    def sort
+        roster.each do |key, value|
+            value.sort!
+        end
+      roster.sort.to_h
+    end
+end
+
+
+#need to take some time to break this one down. With all the different parts and "end" everywhere its confusing me.
